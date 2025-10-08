0bfabafb53a8fd8a167c2142339670856753e875
Avery Lor <averywlor@gmail.com>
2025-10-08 17:39:07 -0400

    correctly formatted

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,23 @@
 #![no_std]
 #![no_main]
 
-
-use core::panic::PanicInfo;
 use core::fmt::Write;
-
+use core::panic::PanicInfo;
 
 mod vga_buffer;
 
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-let mut writer = vga_buffer::WRITER.lock();
-writeln!(writer, "Hello from rust_tiny_os!").unwrap();
-writeln!(writer, "Press Ctrl+C to quit QEMU (or close the window)").unwrap();
+    let mut writer = vga_buffer::WRITER.lock();
+    writeln!(writer, "Hello from rust_tiny_os!").unwrap();
+    writeln!(writer, "Press Ctrl+C to quit QEMU (or close the window)").unwrap();
 
-
-loop {}
+    loop {}
 }
-
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-let mut writer = vga_buffer::WRITER.lock();
-writeln!(writer, "PANIC: {}", info).ok();
-loop {}
+    let mut writer = vga_buffer::WRITER.lock();
+    writeln!(writer, "PANIC: {}", info).ok();
+    loop {}
 }
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,95 +1,134 @@
-use core::fmt;
-use volatile::Volatile;
+// src/vga_buffer.rs
+use core::{fmt, ptr::NonNull};
+use lazy_static::lazy_static;
 use spin::Mutex;
-use core::ptr::Unique;
-
+use volatile::Volatile;
 
 #[allow(dead_code)]
 #[derive(Clone, Copy, Debug)]
 #[repr(u8)]
 pub enum Color {
-Black = 0,
-Blue = 1,
-Green = 2,
-Cyan = 3,
-Red = 4,
-Magenta = 5,
-Brown = 6,
-LightGray = 7,
-DarkGray = 8,
-LightBlue = 9,
-LightGreen = 10,
-LightCyan = 11,
-LightRed = 12,
-Pink = 13,
-Yellow = 14,
-White = 15,
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    Pink = 13,
+    Yellow = 14,
+    White = 15,
 }
-
 
 #[derive(Clone, Copy, Debug)]
 #[repr(transparent)]
 struct ColorCode(u8);
 
-
 impl ColorCode {
-fn new(fg: Color, bg: Color) -> ColorCode {
-ColorCode((bg as u8) << 4 | (fg as u8))
+    fn new(fg: Color, bg: Color) -> ColorCode {
+        ColorCode((bg as u8) << 4 | (fg as u8))
+    }
 }
-}
-
 
 #[repr(C)]
 #[derive(Clone, Copy)]
 struct ScreenChar {
-ascii_character: u8,
-color_code: ColorCode,
+    ascii_character: u8,
+    color_code: ColorCode,
 }
-
 
 const BUFFER_HEIGHT: usize = 25;
 const BUFFER_WIDTH: usize = 80;
 
-
 #[repr(transparent)]
 struct Buffer {
-chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
-
 
 pub struct Writer {
-column_position: usize,
-color_code: ColorCode,
-buffer: Unique<Buffer>,
+    column_position: usize,
+    color_code: ColorCode,
+    buffer: NonNull<Buffer>,
 }
-
 
 impl Writer {
-pub fn write_byte(&mut self, byte: u8) {
-match byte {
-b'\n' => self.new_line(),
-byte => {
-if self.column_position >= BUFFER_WIDTH {
-self.new_line();
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),
+            byte => {
+                if self.column_position >= BUFFER_WIDTH {
+                    self.new_line();
+                }
+
+                let row = BUFFER_HEIGHT - 1;
+                let col = self.column_position;
+                let color_code = self.color_code;
+                unsafe {
+                    // SAFETY: buffer points to VGA memory at 0xb8000
+                    let buffer = self.buffer.as_ptr();
+                    (*buffer).chars[row][col].write(ScreenChar {
+                        ascii_character: byte,
+                        color_code,
+                    });
+                }
+                self.column_position += 1;
+            }
+        }
+    }
+
+    fn new_line(&mut self) {
+        // shift every row up by one
+        unsafe {
+            let buffer = self.buffer.as_ptr();
+            for row in 1..BUFFER_HEIGHT {
+                for col in 0..BUFFER_WIDTH {
+                    let sc = (*buffer).chars[row][col].read();
+                    (*buffer).chars[row - 1][col].write(sc);
+                }
+            }
+            self.clear_row(BUFFER_HEIGHT - 1);
+        }
+        self.column_position = 0;
+    }
+
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+        unsafe {
+            let buffer = self.buffer.as_ptr();
+            for col in 0..BUFFER_WIDTH {
+                (*buffer).chars[row][col].write(blank);
+            }
+        }
+    }
 }
 
-
-let row = BUFFER_HEIGHT - 1;
-let col = self.column_position;
-let color_code = self.color_code;
-unsafe {
-self.buffer.as_ptr().as_mut().unwrap().chars[row][col].write(ScreenChar {
-ascii_character: byte,
-color_code,
-});
-}
-self.column_position += 1;
-}
-}
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        for byte in s.bytes() {
+            match byte {
+                // printable ASCII and newline
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                _ => self.write_byte(0xfe),
+            }
+        }
+        Ok(())
+    }
 }
 
-
-fn new_line(&mut self) {
-for row in 1..BUFFER_HEIGHT {
-for col in 0..BUFFER_WIDTH {
-unsafe {
+lazy_static! {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        // SAFETY: 0xb8000 is the VGA text buffer on x86_64 systems
+        buffer: unsafe { NonNull::new_unchecked(0xb8000 as *mut Buffer) },
+    });
+}
